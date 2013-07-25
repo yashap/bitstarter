5a25c25c90f73eae5c51983f3003807533c6ab20
Yasha <ypodeswa@gmail.com>
2013-07-25 15:19:31 +0000

    Updated web.js to read contents of index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
+
 app.use(express.logger());
+var file = fs.readFileSync('index.html');
+var fileText = file.toString();
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fileText);
 });
 
 var port = process.env.PORT || 5000;
