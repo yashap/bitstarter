2d1ae30977adb43c54d6766edbe9c7f6905e5bd1
Yasha <ypodeswa@gmail.com>
2013-07-25 15:28:06 +0000

    Removed weird changes

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var file = fs.readFileSync('index.html');
 var fileText = file.toString();
 
 app.get('/', function(request, response) {
-  response.send(fileText + "yasha");
+  response.send(fileText);
 });
 
 var port = process.env.PORT || 5000;
